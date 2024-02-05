038c0e41edd89b5162f1488fc8c3de65ecf622ff
otompo <abagnasalifusasco@gmail.com>
2024-02-05 09:55:27 +0000

    first commit

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use(cors());
